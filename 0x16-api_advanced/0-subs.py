66e56a829295c4a6f4ff2126cb266ee56d4b064a
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2024-08-15 02:04:21 +0300

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
 """
-Contains the number_of_subscribers function
+Module 0-subs
+This module defines a function to query the Reddit API and return the number of
+subscribers for a given subreddit.
 """
 
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit"""
-    if not isinstance(subreddit, str) or not subreddit:
-        return 0
+    """
+    Queries the Reddit API and returns the number of subscribers for a given subreddit.
     
+    Args:
+        subreddit (str): The name of the subreddit.
+    
+    Returns:
+        int: The number of subscribers. Returns 0 if the subreddit is invalid.
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)'}
-    
+    headers = {'User-Agent': 'your_custom_user_agent'}
+
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
-            data = response.json().get('data', {})
-            return data.get('subscribers', 0)
+            data = response.json()
+            return data.get('data', {}).get('subscribers', 0)
+        else:
+            return 0
     except requests.RequestException:
-        pass
-
-    return 0
+        return 0
