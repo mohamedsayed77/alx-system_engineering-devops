1b18429f29a7cb6f6a09668b49315fc45e748438
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2024-08-23 00:30:56 +0300

    web stack debugging 4

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,11 @@
-# Increase the ULIMIT for Nginx
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/^ULIMIT=.*/ULIMIT=4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/:/usr/sbin/'
+# Ensure ULIMIT setting is configured for Nginx
+exec { 'set-ulimit-for-nginx':
+  command => 'grep -q "^ULIMIT=" /etc/default/nginx && sed -i "s/^ULIMIT=.*/ULIMIT=4096/" /etc/default/nginx || echo "ULIMIT=4096" >> /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/:/usr/sbin/',
 } ->
 
 # Restart Nginx to apply the changes
 exec { 'nginx-restart':
   command => 'service nginx restart',
-  path    => '/usr/local/bin/:/bin/:/usr/sbin/'
+  path    => '/usr/local/bin/:/bin/:/usr/sbin/',
 }
